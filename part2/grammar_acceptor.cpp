98c67aeb1ea04345095d9d567fa26ff977536aa5
maxwelltrotter <Max_Trotter@csu.fullerton.edu>
2022-09-13 11:30:45 -0700

    Recursive Functions declared and fully implemented

--- part2/grammar_acceptor.cpp
+++ part2/grammar_acceptor.cpp
@@ -3,7 +3,11 @@
 
 #include <iostream>
 
-    std::string word_in_progress;
+std::string word_in_progress;
+std::string S(std::string w, int pos);
+std::string B(std::string w, int pos);
+std::string C(std::string w, int pos);
+std::string D(std::string w, int pos);
 
 // State S defined as a recursive function
     std::string S(std::string w, int pos) {
@@ -14,34 +18,14 @@
                                          // (this is only because state S is non-terminal)
             word_in_progress = "INVALID";
             return "";
-        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S
+        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S again
             return "a" + S(w, (pos + 1));
         } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B
-            return "a" + B(w, (pos + 1));
+            return "b" + B(w, (pos + 1));
         } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state C
-            return "a" + C(w, (pos + 1));
+            return "c" + C(w, (pos + 1));
         } else {
-            word_in_progress = "INVALID";  // Recursive case: any other garbage input indicates failure
-            return "";
-        }
-    };
-
-    std::string A(std::string w, int pos) {
-        if (pos == w.length()) {  // Base case: the word ends without a '$'; indicate failure
-            word_in_progress = "INVALID";
-            return "";
-        } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; indicate failure
-                                         // (this is only because state S is non-terminal)
-            word_in_progress = "INVALID";
-            return "";
-        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S
-            return "a" + S(w, (pos + 1));
-        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B
-            return "a" + B(w, (pos + 1));
-        } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state C
-            return "a" + C(w, (pos + 1));
-        } else {
-            word_in_progress = "INVALID";  // Recursive case: any other garbage input indicates failure
+            word_in_progress = "INVALID";  // Base case: any other garbage input indicates failure
             return "";
         }
     };
@@ -50,18 +34,16 @@
         if (pos == w.length()) {  // Base case: the word ends without a '$'; indicate failure
             word_in_progress = "INVALID";
             return "";
-        } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; indicate failure
-                                         // (this is only because state S is non-terminal)
-            word_in_progress = "INVALID";
-            return "";
-        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S
-            return "a" + S(w, (pos + 1));
-        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B
-            return "a" + B(w, (pos + 1));
-        } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state C
+        } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; begin unwinding (state B is terminal)
+            return "$";
+        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state C
             return "a" + C(w, (pos + 1));
+        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B again
+            return "b" + B(w, (pos + 1));
+        } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state D
+            return "c" + D(w, (pos + 1));
         } else {
-            word_in_progress = "INVALID";  // Recursive case: any other garbage input indicates failure
+            word_in_progress = "INVALID";  // Base case: any other garbage input indicates failure
             return "";
         }
     };
@@ -70,18 +52,16 @@
         if (pos == w.length()) {  // Base case: the word ends without a '$'; indicate failure
             word_in_progress = "INVALID";
             return "";
-        } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; indicate failure
-                                         // (this is only because state S is non-terminal)
-            word_in_progress = "INVALID";
-            return "";
+        } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; begin unwinding (state C is terminal)
+            return "$";
         } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S
             return "a" + S(w, (pos + 1));
-        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B
-            return "a" + B(w, (pos + 1));
-        } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state C
-            return "a" + C(w, (pos + 1));
+        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state D
+            return "b" + D(w, (pos + 1));
+        } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state D
+            return "c" + D(w, (pos + 1));
         } else {
-            word_in_progress = "INVALID";  // Recursive case: any other garbage input indicates failure
+            word_in_progress = "INVALID";  // Base case: any other garbage input indicates failure
             return "";
         }
     };
@@ -91,13 +71,13 @@
             word_in_progress = "INVALID";
             return "";
         } else if (w[pos + 1] == '$') {  // Base case: the word ends with a '$'; indicate failure
-                                         // (this is only because state S is non-terminal)
+                                         // (this is only because state D is non-terminal)
             word_in_progress = "INVALID";
             return "";
-        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state S
-            return "a" + S(w, (pos + 1));
-        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state B
+        } else if (w[pos + 1] == 'a') {  // Recursive case: the word follows the grammar to advance to state B
             return "a" + B(w, (pos + 1));
+        } else if (w[pos + 1] == 'b') {  // Recursive case: the word follows the grammar to advance to state D
+            return "a" + D(w, (pos + 1));
         } else if (w[pos + 1] == 'c') {  // Recursive case: the word follows the grammar to advance to state C
             return "a" + C(w, (pos + 1));
         } else {
@@ -105,7 +85,7 @@
             return "";
         }
     };
-    
+
 int main(){
 
     std::string word;
