3a67a2b44934666060ff21d35d3e1e85b48f2195
maxwelltrotter <Max_Trotter@csu.fullerton.edu>
2022-09-13 10:23:14 -0700

    Interfacing prompt

--- part2/grammar_acceptor.cpp
+++ part2/grammar_acceptor.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+
 int main(){
+
+    std::string word;
+    std::cout << "Welcome to Grammar Acceptor by Maxwell Trotter\n";
+    std::cout << "Please input a string to be checked against the grammar: \n";
+    std::cin >> word;
     return 0;
 }
